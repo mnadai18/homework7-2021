113f6b07d4f1af07488adbcc5490694a65212b0f
McKinley Nadai <78754874+mnadai18@users.noreply.github.com>
2021-11-18 13:20:18 -0500

    requirements

---
+++ js/video.js
@@ -0,0 +1,60 @@
+var video = document.querySelector("video")
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+ document.getElementById('play').addEventListener("click", function() {
+	 console.log("Play Video");
+	 video.play();
+	 document.getElementById('volume').innerHTML = "100";
+ });
+
+ document.getElementById('pause').addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.getElementById('slower').addEventListener("click", function() {
+	video.playbackRate = video.playbackRate - 0.05;
+	console.log("New speed is " + video.playbackRate);
+});
+
+document.getElementById('faster').addEventListener("click", function() {
+	video.playbackRate = video.playbackRate + 0.05;
+	console.log("New speed is " + video.playbackRate);
+});
+
+document.getElementById('mute').addEventListener("click", function() {
+	if (video.muted === false) {
+		video.muted = true;
+		document.getElementById('volume').innerHTML = "0";
+		document.getElementById('mute').innerHTML = "Unmute";
+	}
+	else {
+		video.muted = false;
+		document.getElementById('volume').innerHTML = "100";
+		document.getElementById('mute').innerHTML = "Mute";
+	}
+});
+
+document.getElementById('skip').addEventListener("click", function() {
+	console.log("Original location " + video.currentTime);
+	video.currentTime = video.currentTime + 15;
+	console.log("New location " +video.currentTime);
+	
+});
+
+document.getElementById('slider').addEventListener("change", function() {
+	video.volume = this.value / 100;
+	document.getElementById('volume').innerHTML = video.volume * 100;
+});
+
+document.getElementById('vintage').addEventListener("click", function() {
+	document.querySelector('video').classList.add('oldSchool');
+});
+
+document.getElementById('orig').addEventListener("click", function() {
+	document.querySelector('video').classList.remove('oldSchool');
+});
